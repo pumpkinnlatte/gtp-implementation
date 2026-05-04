52bbc0b7819f1e4edc18d7f8c91c4f0cacf02903
pumpkinnlatte <2230343@upv.edu.mx>
2026-05-03 23:16:23 -0600

    first file

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//We are implementing the list of algorithms presented on 
+//Combinatorial Analysis of Diagonal, Box, and Greater-Than Polynomials as Packing Functions
+
+//KEY CONCEPTS:
+//1. Packaging: We are relationing each element of a multidimensional array with a unique numeric value. 
+//1.1 This means WE NEED A DATA TYPE to represent the MULTIDIMENSIONAL ARRAY.             
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
